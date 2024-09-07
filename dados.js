8a42bc143c52254812c5b487fe534f1bac660090
Mike-Oliver-Dev <michael88oliver@gmail.com>
2024-09-06 22:25:45 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Imbel MD1 GC",
+        descricao : "A Imbel MD1 GC .380 é uma pistola semiautomática de porte compacto, desenvolvida pela Indústria de Material Bélico do Brasil.",
+        link : "https://www.imbel.gov.br/",
+        imagem: "https://images.tcdn.com.br/img/img_prod/1086362/pistola_imbel_md1_gc_s_adc_380_aco_verde_553_1_c27b2894250dd9a6630ff0f2b8907cd2.jpg", alt:"{{objeto.titulo}}", width:"210", height:"150"
+    },
+    {
+        titulo: "TH380",
+        descricao: "A TH380 é uma pistola semiautomática de calibre .380 ACP, conhecida por sua leveza e fácil manuseio.",
+        link: "https://www.taurusarmas.com.br/",
+        imagem: "https://www.armastore.com.br/img/products/pistola-taurus-th380-calibre-380-acp_1_1200.jpg", alt:"{{objeto.titulo}}", width:"200", height:"200"
+    },
+    {
+        titulo: "Glock G25",
+        descricao: "A Glock G25 é uma pistola semiautomática de calibre .380 ACP, famosa por sua durabilidade e confiabilidade, produzida pela Glock.",
+        link: "https://www.glock.com/",
+        imagem: "https://images.tcdn.com.br/img/img_prod/1086362/90_pistola_glock_g25_380_preta_441_1_c3cd09ff61e953b13cf675e1ca24190c.jpg", alt:"{{objeto.titulo}}", width:"200", height:"215"
+    }
+];
